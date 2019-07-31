9e117e9ec9c178bbb16ddae3a267639c6ec6ae41
masspetracca <masspetracca@gmail.com>
2019-07-31 13:27:21 +0200

    Add files via upload

---
+++ batchAPP.bat
@@ -0,0 +1,30 @@
+cd c:/Python26/sdall/
+c:
+del *.CSV /Q
+del *.DEL /Q
+del *.FF /Q
+del *.PIP /Q
+del *.TXT /Q
+del *.XML /Q
+del *.txt /Q
+del *.csv /Q
+del *.ff /Q
+del *.pip /Q
+del *.xml /Q
+del *.xsd /Q
+del *.zip /Q
+cd C:\Python26\sdall\LstAntiTerrFlrt\
+java -jar LstAntiTerrFlrt.jar
+cd H:\Desktop\Antiterrorismo\01_Requisiti utente\Flussi\allZIP
+ftp ofacftp.treas.gov
+anonymous
+masspetracca@gmail.com
+ls
+cd fac_sdn
+get sdall.zip sdall.zip
+cd .. 
+cd fse_list
+get fseall.zip fseall.zip
+cd .. 
+quit
+
---
+++ batchAPP1.bat
@@ -0,0 +1,5 @@
+cd c:\Python26\
+copy c:\Python26\*.py c:\Python26\sdall /Q
+python pyUNZIP.py
+cd C:\Python26\sdall\LstAntiTerrSom\
+java -jar LstAntiterrSom.jar
